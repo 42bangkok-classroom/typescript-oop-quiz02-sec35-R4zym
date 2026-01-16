be02502b381ed686d8347105446259302f890997
R4zym <yossaran.p6.2.17.ccs@gmail.com>
2026-01-16 10:12:33 +0700

    Update index.ts

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,3 +1,11 @@
 export function getEdgePosts(id: number, title: string) {
   return "id: " + id + " title: " + title;
 }
+
+async function getEdgePosts() {
+  try {
+    const title = await getEdgePosts()
+    cosole.log()
+  } catch (e) {
+    
+  }
