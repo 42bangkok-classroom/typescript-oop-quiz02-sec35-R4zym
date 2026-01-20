bd2b3fba1f6f570eeb02b7cd209c21c053a5fca5
Yossaran Mankongdee <68010925@kmitl.ac.th>
2026-01-20 21:25:48 +0700

    q03

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,19 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
-interface Posts {
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+interface Post {
+  userId: number;
   id: number;
   title: string;
+  body: string;
 }
 
-export async function getPostsByUser(userId: number): Promise<Posts[]> {
+export async function getPostsByUser(userId: number): Promise<{ id: number; title: string }[]> {
   try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts",
-    );
-    const posts = await response.data;
+    const res = await axios.get<Post[]>(url);
+    const posts = res.data;
+
     return posts
-      .filter((post: any) => post.userId === userId)
-      .map((post: any) => ({ id: post.id, title: post.title }));
+      .filter((p) => p.userId === userId)
+      .map(({ id, title }) => ({ id, title }));
   } catch (error) {
     return [];
   }
--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,1 +1,30 @@
-export function mapPostWithCommentCount() {}
+import axios from "axios";
+
+interface Posts {
+  postId: number;
+  title: string;
+  totalComments: number;
+}
+
+export async function mapPostWithCommentCount(): Promise<Posts[]> {
+  try {
+    const response1 = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    const response2 = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments",
+    );
+    const posts = response1.data;
+    const comments = response2.data;
+
+    return posts.map((post: any) => ({
+      postId: post.id,
+      title: post.title,
+      totalComments: comments
+        .filter((c: any) => c.postId === post.id)
+        .length,
+    }));
+  } catch (error) {
+    return [];
+  }
+}
